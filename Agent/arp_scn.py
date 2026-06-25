d4e0072b20dd6f60f7d111735b2c1def892f1007
walid <walidmakdoud12@gmail.com>
2026-06-25 23:30:10 +0100

    Fix the arguments in arpscanner

--- Agent/arp_scn.py
+++ Agent/arp_scn.py
@@ -105,7 +105,7 @@ def arp_scanner(interface, ips):
 
 if __name__ == "__main__":
 	
-	if len(sys.argv != 3):
+	if len(sys.argv) != 3:
 
 		print(f"Usage : python3 {sys.argv[0]} <interface> <network ip range>")
 		sys.exit(1)
